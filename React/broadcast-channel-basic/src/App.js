b2f0669291d3a23055e482d4e2419735b16ddd4c
Sai Balaram <Kapavarapu.SaiBalaram@philips.com>
2021-06-12 02:01:01 +0530

    broadcast working

--- React/broadcast-channel-basic/src/App.js
+++ React/broadcast-channel-basic/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BroadcastChannel } from "broadcast-channel";
+const channel = new BroadcastChannel("foobar");
+channel.onmessage = (msg) => console.log(msg);
+const send = () => channel.postMessage("clicked");
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={send} />
     </div>
   );
 }
